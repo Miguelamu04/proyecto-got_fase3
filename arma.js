19d21f4698151e85b8898ff11986dd776640ec59
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-11-11 19:14:38 +0100

    Update arma.js

--- arma.js
+++ arma.js
@@ -1,10 +1,40 @@
 export class Arma {
-    constructor(nombre, danio) {
-        this.nombre = nombre;
-        this.danio = danio;
+    #nombre;
+    #daño;
+    #tipo;
+
+    constructor(nombre = "", tipo = "", daño = 0){
+        this.#nombre = nombre;
+        this.#tipo = tipo;
+        this.#daño = daño;
     }
 
-    descripcion() {
-        console.log(`Arma: ${this.nombre} | Daño: ${this.danio}`);
+    set nombre(valor){
+        this.#nombre = valor;
     }
+
+    set tipo(valor){
+        this.#tipo = valor;
+    }
+
+    set daño(valor){
+        this.#daño = valor;     
+    }
+
+    get nombre(){
+        return this.#nombre;
+    }
+
+    get daño(){
+        return this.#daño;
+    }
+
+    get tipo(){
+        return this.#tipo;
+    }
+    
+    descripcion() {
+        return `${this.#nombre} (${this.#tipo || "sin tipo"}) causa ${this.#daño} de daño.`;
+    }
+
 }
