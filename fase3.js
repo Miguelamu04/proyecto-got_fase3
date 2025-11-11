f1e56f2dc8a119c254cc60a4342da1f6ad4f8c58
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-11-11 19:16:48 +0100

    Refactor casa and weapon imports and member functions

--- fase3.js
+++ fase3.js
@@ -1,51 +1,52 @@
-import { Casa } from './casa.js';
-import { Arma } from './arma.js';
-import { Guerrero } from './especiales/guerrero.js';
-import { Rey } from './especiales/rey.js';
-import { Consejero } from './especiales/consejero.js';
-import { Batalla } from './especiales/batallas.js';
+import { Arma } from "./clases/arma.js";
+import { Casa } from "./clases/casa.js";
+import { Guerrero } from "./especiales/guerrero.js";
+import { Rey } from "./especiales/rey.js";
+import { Consejero } from "./especiales/consejero.js";
+import { Batalla } from "./clases/batalla.js";
 
+const casaStark = new Casa("Stark", "El inverno se acerca");
+const casaLanister = new Casa("Lannister", "Un Lannister siempre paga sus deudas");
 
-// === Casas ===
-const casaStark = new Casa("Stark", "El invierno se acerca");
-const casaLannister = new Casa("Lannister", "Oye mi rugido");
-// ===Armas ===
 const garra = new Arma("Garra", 90);
 const aguja = new Arma("Aguja", 70);
-const hacha = new Arma("Hacha del Norte", 80);
+const hacha = new Arma("Hacha del norte", 80);
 const espadaLarga = new Arma("Espada Larga", 85);
 const ballesta = new Arma("Ballesta", 75);
 const filoRojo = new Arma("Filo Rojo", 88);
-const lanzaDeCaza = new Arma("Lanza de Caza", 60);
+const lanzaDeCaza = new Arma("Lanza de Casa", 60);
 
 // === GUERREROS DE LA CASA STARK ===
-const jon = new Guerrero("Jon Snow", 25, true, casaStark, garra);
-const arya = new Guerrero("Arya Stark", 18, true, casaStark, aguja);
-const robb = new Guerrero("Robb Stark", 22, false, casaStark, espadaLarga);
-const brienne = new Guerrero("Brienne de Tarth", 30, true, casaStark, hacha);
+
+const jon = new Guerrero("Jon Snow", 25, true, "Stark", garra);
+const arya = new Guerrero("Arya Stark", 18, true, "Stark", aguja);
+const robb = new Guerrero("Robb Stark", 22, false, "Stark", espadaLarga);
+const brienne = new Guerrero("Briene de Tarth", 30, true, "Stark", hacha);
 
 // === GUERREROS DE LA CASA LANNISTER ===
-const jaime = new Guerrero("Jaime Lannister", 32, true, casaLannister, filoRojo);
-const cersei = new Guerrero("Cersei Lannister", 34, true, casaLannister, lanzaDeCaza);
-const tyrion = new Guerrero("Tyrion Lannister", 30, true, casaLannister, ballesta);
-const gregor = new Guerrero("Gregor Clegane", 38, true, casaLannister, hacha);
 
-// === AÑADIR PERSONAJES A LAS CASAS ===
+const jaime = new Guerrero("Jaime Lannister", 32, true, "Lannister", filoRojo);
+const cersei = new Guerrero("Cersei Lannister", 34, true, "Lannister", lanzaDeCaza);
+const tyrion = new Guerrero("Tyrion Lannister", 30, true, "Lannister", ballesta);
+const gregor = new Guerrero("Gregor Clegane", 38, true, "Lannister", hacha);
+
+// === AÑADIR PERSONAJES A LA CASAS ===
+
 casaStark.agregarMiembro(jon);
 casaStark.agregarMiembro(arya);
 casaStark.agregarMiembro(robb);
 casaStark.agregarMiembro(brienne);
 
-casaLannister.agregarMiembro(jaime);
-casaLannister.agregarMiembro(cersei);
-casaLannister.agregarMiembro(tyrion);
-casaLannister.agregarMiembro(gregor);
+casaLanister.agregarMiembro(jaime);
+casaLanister.agregarMiembro(cersei);
+casaLanister.agregarMiembro(tyrion);
+casaLanister.agregarMiembro(gregor);
 
-casaStark.mostrarMiembros();
-casaLannister.mostrarMiembros();
-
+casaStark.visualizarMiembros();
+casaLanister.visualizarMiembros();
 
 const batallaBastardos = new Batalla();
-batallaBastardos.iniciarBatalla(casaStark,casaLannister);
+console.log(`⚔️ ¡Comienza la batalla! ⚔️`);
+batallaBastardos.iniciarBatalla(casaStark,casaLanister);
 
-batallaBastardos.mostrarGuerrerosMuertosEnBatalla();
+batallaBastardos.mostrarGuerrerosMuertosEnCombate();
